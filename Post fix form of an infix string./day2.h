60f5818d20f9aefbb4aebac0ca45612f42d128d4
Ashi Sinha <56842767+ashiabcd@users.noreply.github.com>
2020-08-02 22:13:27 +0530

    Create day2.h

---
+++ Post fix form of an infix string./day2.h
@@ -0,0 +1,14 @@
+/*
+Group no=A12
+Group members:
+Godhala Meganaa 2017B3A70973H
+Pranavi Marripudi 2018A7PS0507H
+Ashi Sinha 2017B5A71149H
+*/
+// function to print postfix form of an infix string.
+// author : Rashi Jain, 11/10/19
+
+// prints the postfix expression.
+// infix_exp : character array containing infix string. 
+// return : void. 
+void infixToPostfix(char infix_exp[]);
